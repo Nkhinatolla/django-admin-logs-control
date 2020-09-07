9f4d4a33e08d609757eb5b73c634b8be3880c17c
Nurbergen <madiyar.asroma@gmail.com>
2020-09-07 15:14:00 +0600

    git ignored .idea pycache

--- admintools/models.py
+++ admintools/models.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 def get_content_type_for_model(obj):
     # Since this module gets imported in the application's root package,
     # it cannot import models from other applications at the module level.
-    from django.contrib.contenttypes import ContentType
+    from django.contrib import ContentType
     return ContentType.objects.get_for_model(obj, for_concrete_model=False)
 
 
